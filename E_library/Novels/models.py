cc00ca2de4c51f250d3fb5479088ce1d5d7b9717
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-13 19:46:07 +0000

    mod novel model

--- E_library/Novels/models.py
+++ E_library/Novels/models.py
@@ -2,18 +2,19 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
-        biography = models.TextField(blank=True)
-            date_of_birth = models.DateField(null=True, blank=True)
-                date_of_death = models.DateField(null=True, blank=True)
+    biography = models.TextField(blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
 
-                    def __str__(self):
-                            return self.name
+    def __str__(self):
+       return self.name
 
-                            class Genre(models.Model):
-                                name = models.CharField(max_length=100, unique=True)
 
-                                    def __str__(self):
-                                            return self.name
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
                                             class Novel(models.Model):
                                                 title = models.CharField(max_length=255)
