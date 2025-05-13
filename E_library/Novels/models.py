a26fb9c57fce7d0ff9d2fa667c8edc3216a2b967
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-13 19:42:58 +0000

    novels model

--- E_library/Novels/models.py
+++ E_library/Novels/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+        biography = models.TextField(blank=True)
+            date_of_birth = models.DateField(null=True, blank=True)
+                date_of_death = models.DateField(null=True, blank=True)
+
+                    def __str__(self):
+                            return self.name
+
+                            class Genre(models.Model):
+                                name = models.CharField(max_length=100, unique=True)
+
+                                    def __str__(self):
+                                            return self.name
+
+                                            class Novel(models.Model):
+                                                title = models.CharField(max_length=255)
+                                                    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='novels')
+                                                        genres = models.ManyToManyField(Genre, related_name='novels')
+                                                            summary = models.TextField()
+                                                                isbn = models.CharField('ISBN', max_length=13, unique=True)
+                                                                    cover_image = models.ImageField(upload_to='novel_covers/', null=True, blank=True)
+                                                                        publication_date = models.DateField(null=True, blank=True)
+                                                                            added_on = models.DateTimeField(auto_now_add=True)
+
+                                                                                def __str__(self):
+                                                                                        return self.title
