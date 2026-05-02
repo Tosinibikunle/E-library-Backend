058e83af7c22d65aa367a8e049bf3c651358fdc4
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-05-02 22:43:40 +0100

    Fix indentation in Novel model definition

--- E_library/Novels/models.py
+++ E_library/Novels/models.py
@@ -16,15 +16,15 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
-                                            class Novel(models.Model):
-                                                title = models.CharField(max_length=255)
-                                                    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='novels')
-                                                        genres = models.ManyToManyField(Genre, related_name='novels')
-                                                            summary = models.TextField()
-                                                                isbn = models.CharField('ISBN', max_length=13, unique=True)
-                                                                    cover_image = models.ImageField(upload_to='novel_covers/', null=True, blank=True)
-                                                                        publication_date = models.DateField(null=True, blank=True)
-                                                                            added_on = models.DateTimeField(auto_now_add=True)
+class Novel(models.Model):
+   title = models.CharField(max_length=255)
+   author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='novels')
+   genres = models.ManyToManyField(Genre, related_name='novels')
+   summary = models.TextField()
+   isbn = models.CharField('ISBN', max_length=13, unique=True)
+   cover_image = models.ImageField(upload_to='novel_covers/', null=True, blank=True)
+   publication_date = models.DateField(null=True, blank=True)
+   added_on = models.DateTimeField(auto_now_add=True)
 
-                                                                                def __str__(self):
-                                                                                        return self.title
+   def __str__(self):
+     return self.title
